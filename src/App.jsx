7edf61246cdee98920993092beae61dcaf6e408e
Erickuby <ericcnwankwo@gmail.com>
2023-03-08 20:32:06 +0000

    Error

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './components/about/About'
 import Contact from './components/contact/Contact'
 import Experience from './components/experience/Experience'
 import Portfolio from './components/portfolio/Portfolio'
-import Services from './components/services/Services'
 import Testimonials from './components/testimonials/Testimonials'
 import Footer from './components/footer/Footer'
 
